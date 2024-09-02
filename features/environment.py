3a5168c18afb6c89ccb16dfb5e485d1a5b104429
Sajeev Ezhapilli <shruti_17_preman@hotmail.com>
2024-09-02 16:18:04 -0500

    cloud testing added

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 from webdriver_manager.firefox import GeckoDriverManager
 from app.application import Application
 
-def browser_init(context):
+
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
@@ -20,20 +22,36 @@ def browser_init(context):
     #context.driver = webdriver.Firefox(service=service)
 
     # Headless
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    service = Service(ChromeDriverManager().install())
-    context.driver = webdriver.Chrome(options=options, service=service)
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('headless')
+    #service = Service(ChromeDriverManager().install())
+    #context.driver = webdriver.Chrome(options=options, service=service)
+
+    ### BROWSERSTACK ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'shrutiprem_ndUArm'
+    bs_key = 'Xzst8mjdg4ExRqZLAyS1'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os": "OS X",
+        "osVersion": "Monterey",
+        'browserName': 'edge',
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
 
-
     context.app = Application(context.driver)
+
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
